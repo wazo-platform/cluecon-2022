ecc06edb8cfaaa1ead55f1dd9dd71bb9baa9e770
Sylvain Boily <sylvain@wazo.io>
2022-10-19 14:14:49 -0500

    add video token support

--- server/main.py
+++ server/main.py
@@ -12,9 +12,10 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 db = TinyDB('db.json')
 
-URL = 'https://wazo.signalwire.com/api/chat/tokens'
+PROJECT_NAME = os.getenv('SIGNALWIRE_PROJECT_NAME')
 PROJECT_ID = os.getenv('SIGNALWIRE_PROJECT_ID')
 API_KEY = os.getenv('SIGNALWIRE_API_KEY')
+URL = f'https://{PROJECT_NAME}.signalwire.com'
 
 @app.route('/')
 def main():
@@ -54,7 +55,22 @@ def add_channels():
 @app.route('/api/chat/tokens', methods=['POST'])
 def chan_token():
     response = requests.post(
-        URL,
+        f'{URL}/api/chat/tokens',
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        json=request.json,
+        auth=HTTPBasicAuth(PROJECT_ID, API_KEY),
+    )
+
+    return response.text, response.status_code
+
+
+@app.route('/api/video/tokens', methods=['POST'])
+def video_token():
+    response = requests.post(
+        f'{URL}/api/video/room_tokens',
         headers = {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
